a40f0c7f65148fe6a8a49270e28bf70c426950fd
DiranDesai <dirantechie@gmail.com>
2025-04-18 23:21:30 +0200

    Updating codebase

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
-function ProfileMenu({Icon, title}) {
+function ProfileMenu({Icon, title, ShowArrow=true}) {
   return (
     <div className="flex align-center justify-between mt-4 shadow-sm bg-white p-4 rounded-md hover:shadow-md cursor-pointer transition duration-200 ease-in-out">
         <div className='flex align-center space-x-2'>
             <span className="w-6 h-6 lg:w-10 lg:h-10 self-center">{Icon}</span>
             <h2 className="text-[16px] text-[#181818] font-[500] self-center">{title}</h2>
         </div>
-        <ArrowRightIcon  className="h-6 w-6 font-bold text-gray-500 self-center" />  
+        {ShowArrow ? <ArrowRightIcon  className="h-6 w-6 font-bold text-gray-500 self-center" /> : <p className="font-extrabold">150</p>}  
     </div>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -22,11 +22,10 @@ function Profile() {
                 <PencilSquareIcon className="h-7 w-7 font-bold bg-blue-400 text-white rounded-full p-2 hover:bg-red absolute right-0 top-5" />
                </div>
                 <h2 className="text-md font-[500]">Oladele Joshua</h2>
-                <p className="font-bold text-xl font-bold bg-blue-400 text-white scale-75 p-2 rounded-md">Mechnical Engineer</p>
-                
+                <p className="font-extrabold">+260776047932</p>
             </div>
         </div>
-        <ProfileMenu Icon={<CheckBadgeIcon />} title="Credit limit"/>
+        <ProfileMenu Icon={<CheckBadgeIcon />} title="Credit limit" ShowArrow={false}/>
         <ProfileMenu Icon={<CalendarDaysIcon />} title="Bank cards"/>
         <ProfileMenu Icon={<UserGroupIcon />} title="Invite friends"/>
         <ProfileMenu Icon={<LockClosedIcon />} title="Account settings"/>
