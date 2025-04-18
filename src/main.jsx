5b2765a5a2d04984e6e81fc6af69b59fd4c0d422
DiranDesai <dirantechie@gmail.com>
2025-04-18 10:43:31 +0200

    Updating codebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import AuthState from './context/authContext/AuthState';
+import AuthState from './context/authContext/authState';
 import './index.css'
 import App from './App.jsx'
 
@@ -12,5 +12,5 @@ createRoot(document.getElementById('root')).render(
         <App />
       </AuthState>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
