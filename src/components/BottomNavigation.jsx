750dadf5e4ebc3235559772e1ee6708892e44583
DiranDesai <dirantechie@gmail.com>
2025-04-18 23:57:08 +0200

    Updating codebase

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -5,32 +5,42 @@ import {
     ClipboardDocumentListIcon,
   } from "@heroicons/react/24/outline";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
   
   function BottomNavigation() {
+    const location = useLocation();
+
+    const navItems = [
+      { to: "/", label: "Home", icon: HomeIcon },
+      { to: "/loan", label: "Get Loan", icon: BanknotesIcon },
+      { to: "/loan-profile", label: "Loan Profile", icon: ClipboardDocumentListIcon },
+      { to: "/profile", label: "Profile", icon: UserCircleIcon },
+    ];
+
     return (
       <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md border-t border-gray-200 z-50">
         <div className="flex justify-around items-center py-2">
-          <Link to="/">
-            <NavItem icon={<HomeIcon className="h-6 w-6" />} label="Home" />
+        {navItems.map(({ to, label, icon: Icon }) => (
+          <Link to={to} key={label}>
+            <NavItem
+              icon={<Icon className="h-7 w-7" />}
+              label={label}
+              active={location.pathname === to}
+            />
           </Link>
-          <Link to="/loan">
-            <NavItem icon={<BanknotesIcon className="h-6 w-6" />} label="Get Loan" />
-          </Link>
-          <Link>
-            <NavItem icon={<ClipboardDocumentListIcon className="h-6 w-6" />} label="Loan Profile" />
-          </Link>
-          <Link to="/profile">
-            <NavItem icon={<UserCircleIcon className="h-6 w-6" />} label="Profile" />
-          </Link>
+        ))}
         </div>
       </div>
     );
   }
   
-  function NavItem({ icon, label }) {
+  function NavItem({ icon, label, active }) {
     return (
-      <div className="flex flex-col items-center text-gray-600 hover:text-blue-500 cursor-pointer transition">
+      <div
+        className={`flex flex-col items-center ${
+          active ? "text-blue-500" : "text-gray-600"
+        } hover:text-blue-500 cursor-pointer transition`}
+      >
         {icon}
         <span className="text-xs mt-1">{label}</span>
       </div>
