54604854a798ae896276d3e61d295458d4146bd0
DiranDesai <dirantechie@gmail.com>
2025-04-20 07:33:14 +0200

    Updating codebase

--- src/components/ProfileBanner.jsx
+++ src/components/ProfileBanner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import admin from "../assets/admin.jpg"
+import user1 from "../assets/user1.png"
 
 function ProfileBanner() {
   return (
@@ -9,7 +10,7 @@ function ProfileBanner() {
             <h2 className="font-extrabold text-2xl text-[rgb(41,51,63)]">Hello Diran</h2>
             <p className="text-[#6f7b87]">Lets empower your business today</p>
         </div>
-        <Link to="/profile"><img src={admin} className="rounded-full w-10 h-10" alt="" /></Link>
+        <Link to="/profile"><img src={user1} className="rounded-full w-10 h-10" alt="" /></Link>
     </div>
   )
 }
