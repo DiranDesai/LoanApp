fd04e699053a50f369223cb6b2649207c1a16cf3
DiranDesai <dirantechie@gmail.com>
2025-04-20 08:02:49 +0200

    Updating codebase

--- src/components/LoanBanner.jsx
+++ src/components/LoanBanner.jsx
@@ -7,7 +7,7 @@ function LoanBanner() {
     <div className="p-5 mt-6 rounded-md bg-red-500 loan-banner">
         <p className="text-white">Available Loan</p>
         <h2 className="text-3xl text-white font-extrabold">K1200</h2>
-        <Link to="/apply"><button type="button" className="flex rounded-full bg-white px-4 py-3 mt-3 font-bold"><PencilSquareIcon className="w-6 h-6"/> Apply Loan</button></Link>
+        <Link to="/apply"><button type="button" className="flex rounded-full bg-white px-4 py-3 mt-5 font-bold"><PencilSquareIcon className="w-6 h-6"/> Apply Loan</button></Link>
     </div>
   )
 }
