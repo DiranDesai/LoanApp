c98f9db9866c809e72f3be50a5d3615a31161a00
DiranDesai <dirantechie@gmail.com>
2025-04-20 21:52:14 +0200

    Updating codebase

--- src/components/ProfileBanner.jsx
+++ src/components/ProfileBanner.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import admin from "../assets/admin.jpg"
-import user1 from "../assets/user1.png"
-import {BellAlertIcon} from '@heroicons/react/24/outline';
+import React from "react";
+import { Link } from "react-router-dom";
+import admin from "../assets/admin.jpg";
+import user1 from "../assets/user1.png";
+import { BellAlertIcon } from "@heroicons/react/24/outline";
 
 function ProfileBanner() {
   return (
     <div className="flex items-center justify-between">
-        <div className="flex items-center justify-between">
-            <Link to="/profile"><img src={user1} className="rounded-full w-[60px] h-[60px]" alt="" /></Link>
-            <div>
-              <p className="text-[#6f7b87]">Happy Monday!</p>
-              <h2 className="font-bold text-2xl text-[rgb(41,51,63)]">Hello Diran</h2>
-            </div>
+      <div className="flex items-center justify-between">
+        <Link to="/profile">
+          <img src={user1} className="rounded-full w-[60px] h-[60px]" alt="" />
+        </Link>
+        <div>
+          <p className="text-[#6f7b87]">Happy Monday!</p>
+          <h2 className="font-bold text-2xl text-[rgb(41,51,63)]">
+            Hello Diran
+          </h2>
         </div>
-        <BellAlertIcon className="w-10 h-10"/>
+      </div>
+      <div className="relative inline-block">
+        <BellAlertIcon className="w-10 h-10 text-gray-700" />
+        <span className="absolute top-0 right-0 -mt-1 -mr-1 bg-red-600 text-white text-xs font-bold px-1.5 py-0.5 rounded-full">
+          3
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProfileBanner
+export default ProfileBanner;
