f28f8f36e688b15c66ebcedff28025f6806dff79
DiranDesai <dirantechie@gmail.com>
2025-04-20 23:03:35 +0200

    Updating codebase

--- src/context/authContext/authState.jsx
+++ src/context/authContext/authState.jsx
@@ -10,7 +10,7 @@ const AuthState = ({children}) => {
     const initialState = {
         user: {name: "diran"},
         loading: false,
-        token: 1234252,
+        token: null,
         error: null
     };
 
--- src/pages/ApplyLoan.jsx
+++ src/pages/ApplyLoan.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import BottomNavigation from '../components/BottomNavigation'
+import Personalnformation from '../components/Personalnformation'
 
 function ApplyLoan() {
   return (
     <>
-        <div>ApplyLoan</div>
+        <div className="p-5">
+          <Personalnformation />
+        </div>
         <BottomNavigation />
     </>
   )
