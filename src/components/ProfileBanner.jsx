99a7cbb85ca2f4f20ad7947bf51609162ec27c7a
DiranDesai <dirantechie@gmail.com>
2025-04-20 07:50:09 +0200

    Updating codebase

--- src/components/ProfileBanner.jsx
+++ src/components/ProfileBanner.jsx
@@ -10,7 +10,7 @@ function ProfileBanner() {
             <h2 className="font-extrabold text-2xl text-[rgb(41,51,63)]">Hello Diran</h2>
             <p className="text-[#6f7b87]">Lets empower your business today</p>
         </div>
-        <Link to="/profile"><img src={user1} className="rounded-full w-15 h-15" alt="" /></Link>
+        <Link to="/profile"><img src={user1} className="rounded-full w-[40px] h-[40px]" alt="" /></Link>
     </div>
   )
 }
