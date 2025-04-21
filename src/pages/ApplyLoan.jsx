270f3cf3eb7de2faefd7620ed738d823a2db5d54
DiranDesai <dirantechie@gmail.com>
2025-04-21 07:30:40 +0200

    Updating codebase

--- src/pages/ApplyLoan.jsx
+++ src/pages/ApplyLoan.jsx
@@ -10,7 +10,7 @@ function ApplyLoan() {
           <Personalnformation />
         </div>
         <BottomNavigation />
-        <NotifyDialog message="Something went wrong..."/>
+        {/* <NotifyDialog message="Something went wrong..."/> */}
     </>
   )
 }
