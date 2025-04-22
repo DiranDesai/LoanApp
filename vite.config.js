d58c22a9d5caf5f0190c3240af916c72cdc39cce
DiranDesai <dirantechie@gmail.com>
2025-04-22 01:05:54 -0700

    Upading codebase

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate', // or 'prompt'
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'LoanApp',
+      short_name: 'LoanApp',
+      description: 'An awesome lending platform',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/pwa-icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/pwa-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })]
 })
