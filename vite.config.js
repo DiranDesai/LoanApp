410b565ea0c07e80817175ac51de43dfbe8c9fe8
DiranDesai <dirantechie@gmail.com>
2025-04-22 02:21:55 -0700

    Upading codebase

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate', // or 'prompt'
-    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-    manifest: {
-      name: 'LendSia',
-      short_name: 'LendSia',
-      description: 'An awesome lending platform',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: '/pwa-icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/pwa-icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
-  })]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // or 'prompt'
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "LendSia",
+        short_name: "LendSia",
+        description: "An awesome lending platform",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        form_factor: "mobile",
+        screenshots: [
+          {
+            src: "/screenshot.png",
+            sizes: "360x640",
+            type: "image/jpeg",
+          },
+        ],
+      },
+    }),
+  ],
+});
