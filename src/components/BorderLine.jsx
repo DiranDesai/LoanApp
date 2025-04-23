14fd8e869786f19d6545f7920bb789686f02e907
DiranDesai <dirantechie@gmail.com>
2025-04-23 00:46:47 -0700

    Upading codebase

---
+++ src/components/BorderLine.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BorderLine() {
+  return (
+    <div className="border-b my-4"></div>
+  )
+}
+
+export default BorderLine
--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import BorderLine from './BorderLine';
 
 function ProfileMenu({Icon, title, ShowArrow=true}) {
   return (
-    <div className="flex align-center justify-between mt-4 shadow-sm bg-white p-4 rounded-md hover:shadow-md cursor-pointer transition duration-200 ease-in-out">
+    <div className="flex align-center justify-between mt-4 bg-white p-4 rounded-md hover:shadow-md cursor-pointer transition duration-200 ease-in-out">
         <div className='flex align-center space-x-2'>
             <span className="w-6 h-6 lg:w-10 lg:h-10 self-center">{Icon}</span>
             <h2 className="text-[16px] text-[#181818] font-[500] self-center">{title}</h2>
